b84c98d6cd2755131e70ac9edf837ccd771c84fa
uhuikim <uhui94@naver.com>
2023-11-01 14:53:09 +0900

    chore : select 타입스크립트 코드 추가

--- src/utils/selectProducts.ts
+++ src/utils/selectProducts.ts
@@ -1,5 +1,14 @@
 import { Product } from "../types/Product";
 
+// 추상화 하면 이렇게 짤 수 있는 것 같다!! 타입스크립트 공부해보기
+// export default function select<T1 extends object, T2>(
+//   items: T1[],
+//   field: string,
+//   value: T2,
+// ): T1[] {
+//   return items.filter((item) => Reflect.get(item, field) === value);
+// }
+
 export default function selectProducts(
   items: Product[],
   category: string
